05597b71cc99734143d3c3f230804228ddd29153
Daniel Dante <daniel.dante@me.com>
2026-03-30 17:09:18 -0300

    Add files via upload
    Novo API X twitter

--- worker.py
+++ worker.py
@@ -4,7 +4,7 @@ from supabase import create_client, Client
 import tweepy
 
 # ================== CREDENCIAIS ==================
-X_BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAANiR8gEAAAAAdta99ULxZosQ2%2BQ6bI06hkYCokM%3Dz1CVilTZStkeSUvf25zO49m7zTcgwPlLhz8bX7QsI8RrDbX4dY"
+X_BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAALGh8gEAAAAAT07x5eE2NqRkAgy9ixSYAyfk0y0%3DrVk794xJCZHSQRSGj5zeMOwdGkvE2uKWYj9KvEn5JfMd9D17ug"
 
 SUPABASE_URL = "https://mncnvmalmxpbiojcqqjo.supabase.co"
 SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1uY252bWFsbXhwYmlvamNxcWpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4OTE1MTQsImV4cCI6MjA5MDQ2NzUxNH0.lBgvhixDr98ne1HuqeY0YFzXRyAb2yoRRH1Fn74Z0vk"
